6da41afebc4fea0cce26a1cfc2dc262ab15728eb
rtan4 <raymond.tan@illumina.com>
2024-11-27 20:05:48 +0800

    Added ngb to project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarPoolComponent } from "./car-pool/car-pool.component";
+import { ExpertComponent } from "./expert/expert.component";
+import { BondHubComponent } from "./bond-hub/bond-hub.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path:'car-pool',component: CarPoolComponent},
+    {path:'expert',component: ExpertComponent},
+    {path:'bond',component: BondHubComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CarPoolComponent } from './car-pool/car-pool.component';
+import { BondHubComponent } from './bond-hub/bond-hub.component';
+import { ExpertComponent } from './expert/expert.component';
+import { FormsModule } from "@angular/forms";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    declarations: [
+        AppComponent,
+        CarPoolComponent,
+        BondHubComponent,
+        ExpertComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        NgbModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AppModule { }
+export class AppModule {
+}
